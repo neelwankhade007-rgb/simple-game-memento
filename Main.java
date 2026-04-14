27f466d9f943852898fa2cda285d6352c1c0b293
Neel Wankhade <neelwankhade007@gmail.com>
2026-04-14 16:56:40 +0530

    Minor bug fixes related to save and load

--- Main.java
+++ Main.java
@@ -15,6 +15,7 @@ public class Main {
 
             switch (choice) {
                 case 1:
+                    game = new Game();
                     game.play();
                     while (true) {
                         System.out.println("\n1. Damage   2. Save   3. Status   4. Exit");
@@ -56,12 +57,56 @@ public class Main {
                     if (manager.listSaves()) {
                         System.out.print("Enter save index: ");
                         int idx = sc.nextInt();
+
                         GameState state = manager.getSave(idx);
-                        if (state != null) {
-                            game.restore(state);
+
+                        if (state == null) {
+                            System.out.println("\n--- INVALID SAVE INDEX ---");
+                            break;
                         }
-                    } else
+
+                        game.restore(state);
+
+                        while (true) {
+                            System.out.println("\n1. Damage   2. Save   3. Status   4. Exit");
+                            System.out.print("Choose: ");
+                            int subchoice = sc.nextInt();
+
+                            switch (subchoice) {
+                                case 1:
+                                    boolean alive = game.playTurn();
+                                    if (!alive) {
+                                        System.out.println("\n--- GAME OVER ---");
+                                        System.out.println("Exiting Game...");
+                                        flag = 1;
+                                    }
+                                    break;
+
+                                case 2:
+                                    manager.addSave(game.save());
+                                    break;
+
+                                case 3:
+                                    game.showStatus();
+                                    break;
+
+                                case 4:
+                                    flag = 1;
+                                    break;
+
+                                default:
+                                    System.out.println("Invalid choice");
+                            }
+
+                            if (flag == 1) {
+                                flag = 0;
+                                break;
+                            }
+                        }
+
+                    } else {
                         System.out.println("\n--- NO SAVED GAMES FOUND ---");
+                    }
                     break;
 
                 case 3:
